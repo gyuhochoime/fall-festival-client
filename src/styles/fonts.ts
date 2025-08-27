cdea3e1c1c83de9f8d563923aaa03eaeda9ba62e
DoggiKim <kmss0426@hanyang.ac.kr>
2025-08-28 01:13:53 +0900

    feat: update global font to pretendard

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -2,26 +2,26 @@ import { css } from 'styled-components';
 
 export const fontFaces = css`
   @font-face {
-    font-family: SUIT;
+    font-family: Pretendard;
     font-weight: 400;
-    src: url('/fonts/SUIT-Regular.woff2') format('woff2');
+    src: url('/fonts/Pretendard-Regular.woff2') format('woff2');
   }
 
   @font-face {
-    font-family: SUIT;
+    font-family: Pretendard;
     font-weight: 500;
-    src: url('/fonts/SUIT-Medium.woff2') format('woff2');
+    src: url('/fonts/Pretendard-Medium.woff2') format('woff2');
   }
 
   @font-face {
-    font-family: SUIT;
+    font-family: Pretendard;
     font-weight: 600;
-    src: url('/fonts/SUIT-SemiBold.woff2') format('woff2');
+    src: url('/fonts/Pretendard-SemiBold.woff2') format('woff2');
   }
 
   @font-face {
-    font-family: SUIT;
+    font-family: Pretendard;
     font-weight: 700;
-    src: url('/fonts/SUIT-Bold.woff2') format('woff2');
+    src: url('/fonts/Pretendard-Bold.woff2') format('woff2');
   }
 `;
