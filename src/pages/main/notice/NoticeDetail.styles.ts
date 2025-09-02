a440349f5a6d01a80ea2857a4c254d6f2134066d
pdjdev <pdj5096@gmail.com>
2025-09-02 12:20:32 +0900

    feat: implement notice API endpoint request

--- src/pages/main/notice/NoticeDetail.styles.ts
+++ src/pages/main/notice/NoticeDetail.styles.ts
@@ -9,3 +9,20 @@ export const Container = styled.main`
   padding-top: 3.875rem;
   overflow: hidden;
 `;
+
+// 로딩 및 에러 텍스트 스타일
+export const LoadingText = styled.p`
+  ${(props) => props.theme.fonts.body.medium500};
+  color: ${(props) => props.theme.colors.grayScale.gy700};
+  text-align: center;
+  padding: 2rem;
+  width: 100%;
+`;
+
+export const ErrorText = styled.p`
+  ${(props) => props.theme.fonts.body.medium500};
+  color: ${(props) => props.theme.colors.secondary.rd500};
+  text-align: center;
+  padding: 2rem;
+  width: 100%;
+`;
