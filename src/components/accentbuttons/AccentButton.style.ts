3dfd5b2a8bebeb1808a834e3c37adefedc898929
pdjdev <pdj5096@gmail.com>
2025-09-03 20:01:34 +0900

    fix: change PWA guide popup logic for iOS, rename BlueButton component to AccentButton

--- src/components/accentbuttons/AccentButton.style.ts
+++ src/components/accentbuttons/AccentButton.style.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { StyledButtonProps } from './BlueButton.types';
+import { StyledButtonProps } from './AccentButton.types';
 
 export const StyledButton = styled.button<StyledButtonProps>`
   display: flex;
