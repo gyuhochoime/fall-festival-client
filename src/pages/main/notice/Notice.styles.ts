b5d37146e8bcb2061aa0f2fe2e09e96268a6eb27
pdjdev <pdj5096@gmail.com>
2025-09-05 19:27:26 +0900

    feat: add tabs at notice list page

--- src/pages/main/notice/Notice.styles.ts
+++ src/pages/main/notice/Notice.styles.ts
@@ -10,9 +10,29 @@ export const Container = styled.div`
 export const Flex = styled.div`
   display: flex;
   flex-direction: column;
-  margin-top: 1rem;
   width: 21rem;
   gap: 0.375rem;
+`;
+
+export const NoticeTabsWrapper = styled.div`
+  button[data-active='false'] {
+    background-color: ${({ theme }) => theme.colors.grayScale.gy50} !important;
+    border-color: transparent !important;
+
+    p {
+      color: ${({ theme }) => theme.colors.grayScale.gy800} !important;
+      font-weight: 400;
+    }
+  }
+
+  button[data-active='true'] {
+    background-color: ${({ theme }) => theme.colors.primary.violet} !important;
+
+    p {
+      color: ${({ theme }) => theme.colors.grayScale.white} !important;
+      font-weight: 600;
+    }
+  }
 `;
 
 // 로딩, 에러, 빈 목록 메시지용 스타일
