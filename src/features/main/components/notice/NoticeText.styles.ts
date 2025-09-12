e1ec566ad2171754e4bca03b2e6aa0e28f9db9a9
pdjdev <pdj5096@gmail.com>
2025-09-13 00:27:52 +0900

    fix: update display property for body text to improve compatibility

--- src/features/main/components/notice/NoticeText.styles.ts
+++ src/features/main/components/notice/NoticeText.styles.ts
@@ -51,7 +51,7 @@ export const body = styled.p`
   align-self: stretch;
   overflow: hidden;
   text-overflow: ellipsis;
-  display: box; /* Changed to -webkit-box for better compatibility */
+  display: box;
   -webkit-line-clamp: 2;
   -webkit-box-orient: vertical;
 `;
